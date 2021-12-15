1c0f01224fe94dd4f517e367f3e5d7b401dfe449
Matt <86365526+matthewstack@users.noreply.github.com>
2021-12-15 16:55:47 -0500

    Final touches

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link, useParams, useHistory } from "react-router-dom";
+import DeleteMovieModal from "./DeleteMovieModal";
 
 import axios from "axios";
 
@@ -32,70 +33,70 @@ const Movie = (props) => {
   };
 
   return (
-    <div className="modal-page col">
-      <div className="modal-dialog">
-        <div className="modal-content">
-          <div className="modal-header">
-            <h4 className="modal-title">{movie.title} Details</h4>
-          </div>
-          <div className="modal-body">
-            <div className="flexContainer">
-              <section className="movie-details">
-                <div>
-                  <label>
-                    Title: <strong>{movie.title}</strong>
-                  </label>
-                </div>
-                <div>
-                  <label>
-                    Director: <strong>{movie.director}</strong>
-                  </label>
-                </div>
-                <div>
-                  <label>
-                    Genre: <strong>{movie.genre}</strong>
-                  </label>
-                </div>
-                <div>
-                  <label>
-                    Metascore: <strong>{movie.metascore}</strong>
-                  </label>
-                </div>
-                <div>
-                  <label>Description:</label>
-                  <p>
-                    <strong>{movie.description}</strong>
-                  </p>
-                </div>
-              </section>
+    <div>
+      <div className="modal-page col">
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h4 className="modal-title">{movie.title} Details</h4>
+            </div>
+            <div className="modal-body">
+              <div className="flexContainer">
+                <section className="movie-details">
+                  <div>
+                    <label>
+                      Title: <strong>{movie.title}</strong>
+                    </label>
+                  </div>
+                  <div>
+                    <label>
+                      Director: <strong>{movie.director}</strong>
+                    </label>
+                  </div>
+                  <div>
+                    <label>
+                      Genre: <strong>{movie.genre}</strong>
+                    </label>
+                  </div>
+                  <div>
+                    <label>
+                      Metascore: <strong>{movie.metascore}</strong>
+                    </label>
+                  </div>
+                  <div>
+                    <label>Description:</label>
+                    <p>
+                      <strong>{movie.description}</strong>
+                    </p>
+                  </div>
+                </section>
 
-              <section>
-                <span
-                // className="m-2 btn btn-dark"
-                >
-                  <input
-                    type="button"
-                    className="m-2 btn btn-dark"
-                    value="Favorite"
-                    onClick={() => handleFavorite(movie)}
-                  />
-                </span>
+                <section>
+                  <span>
+                    <input
+                      type="button"
+                      className="m-2 btn btn-dark"
+                      value="Favorite"
+                      onClick={() => handleFavorite(movie)}
+                    />
+                  </span>
 
-                <Link
-                  to={`/movies/edit/${movie.id}`}
-                  className="m-2 btn btn-success"
-                >
-                  Edit
-                </Link>
-                <span className="delete">
-                  <input
-                    type="button"
-                    className="m-2 btn btn-danger"
-                    value="Delete"
-                    onClick={() => handleDelete(id)}
-                  />
-                </span>
-              </section>
+                  <Link
+                    to={`/movies/edit/${movie.id}`}
+                    className="m-2 btn btn-success"
+                  >
+                    Edit
+                  </Link>
+                  <span className="delete">
+                    <input
+                      type="button"
+                      className="m-2 btn btn-danger"
+                      value="Delete"
+                      onClick={() => handleDelete(id)}
+                    />
+                  </span>
+                </section>
+              </div>
             </div>
           </div>
         </div>
